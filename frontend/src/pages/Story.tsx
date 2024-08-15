3671098ff6e12528f9e3ac580ce839ec30fb5e72
re <arkajyotiadhikary15.com>
2024-08-15 21:24:57 +0530

    decision window

--- frontend/src/pages/Story.tsx
+++ frontend/src/pages/Story.tsx
@@ -20,10 +20,7 @@ const Story = () => {
   const [image,setImage] = useState("https://t3.ftcdn.net/jpg/05/74/59/94/360_F_574599444_KdhVklOr3PTUyMlq7079mbrFXyB0lFXf.jpg");
   const [getImage,setGetImage] = useState(true); // To handle do we need AI genererated images or not.
 
-
-  useEffect(()=>{
-    
-    const fetchImage = async (scenario:string) =>{
+  const fetchImage = async (scenario:string) =>{
       try {
         const resImg = await http.post("http://localhost:8080/get-image", {
             prompt: `Create a colorful, imaginative drawing for the scenario "${scenario}" that has a comic-style vibe. The image should look playful and whimsical, with exaggerated features and vibrant colors. Avoid using any realistic objects; instead, focus on creating a fun, comic-inspired scene. #${Math.floor(Math.random() * 10000)}`
@@ -36,10 +33,12 @@ const Story = () => {
 
 
     // fetch the starting story and the options
-    const fetchStory = async() => {
+    const fetchStory = async(scenario:string, gamestat:"start"|"continue"|"end", username?:string ,useroption?: string) => {
       const response = await http.post("http://localhost:8080/story",{
+        scenario,
+        stat: gamestat,
         username,
-        stat:"start"
+        useroption,          
       });
       if(response) {
         setGameDate(response.data.result)
@@ -49,9 +48,15 @@ const Story = () => {
         }
       };
     };
-    
-    fetchStory();
-  
+
+  const handleNext = (e) =>
+  {
+    const useroption = e.target.value;
+    fetchStory(gameData.scenario,"continue",username,useroption);
+  }
+
+  useEffect(()=>{      
+    fetchStory("","start",username,"");
   },[])
 
   return <div className="flex flex-col items-center justify-center h-screen w-full">
@@ -63,7 +68,7 @@ const Story = () => {
       </p>
       <ul className="mt-6 space-y-4">
         {gameData.options.map((option,index)=><li key={index}>
-            <button className="w-full max-w-md px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 focus:outline-none">
+            <button onClick={handleNext} value={option.replace(/^\d+\.\s*/,'')} className="w-full max-w-md px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 focus:outline-none">
               {option.replace(/^\d+\.\s*/,'')}  
             </button>
           </li>)}
