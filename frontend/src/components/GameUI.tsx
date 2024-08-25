8dfb7821dad6cb4106ce4bb3431b893b23aa70bf
arkajyotiadhikary <arkajyotiadhikary15@gmail.com>
2024-08-25 16:32:21 +0530

    game ui make it responsive

--- frontend/src/components/GameUI.tsx
+++ frontend/src/components/GameUI.tsx
@@ -23,32 +23,64 @@ const GameUI: React.FC<IGameUI> = (props) => {
   const { health, wealth, luck } = props.points;
 
   console.log(health, wealth, luck);
+
+
   return (
-    <div className="flex justify-between my-10 p-6 bg-gray-800 rounded-lg shadow-lg">
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 my-10 p-6 bg-gray-800 rounded-lg shadow-lg">
       {/* Health */}
       <div className="flex items-center">
-        <img className="me-4" src={healthIcon} height="30" width="30" alt="Health Icon" />
-        <div className="flex flex-col">
+        <img
+          className="me-4"
+          src={healthIcon}
+          height="30"
+          width="30"
+          alt="Health Icon"
+        />
+        <div className="flex flex-col flex-grow">
           <span className="text-sm text-gray-400 mb-1">Health</span>
-          <Progress w={200} value={health} className="bg-red-500" />
+          <Progress
+            w="full"
+            value={health}
+            className="bg-red-500"
+          />
         </div>
       </div>
 
       {/* Wealth */}
       <div className="flex items-center">
-        <img className="me-4" src={wealthIcon} height="30" width="30" alt="Wealth Icon" />
-        <div className="flex flex-col">
+        <img
+          className="me-4"
+          src={wealthIcon}
+          height="30"
+          width="30"
+          alt="Wealth Icon"
+        />
+        <div className="flex flex-col flex-grow">
           <span className="text-sm text-gray-400 mb-1">Wealth</span>
-          <Progress w={200} value={wealth} className="bg-yellow-500" />
+          <Progress
+            w="full"
+            value={wealth}
+            className="bg-yellow-500"
+          />
         </div>
       </div>
 
       {/* Luck Point */}
       <div className="flex items-center">
-        <img className="me-4" src={luckIcon} height="30" width="30" alt="Luck Icon" />
-        <div className="flex flex-col">
+        <img
+          className="me-4"
+          src={luckIcon}
+          height="30"
+          width="30"
+          alt="Luck Icon"
+        />
+        <div className="flex flex-col flex-grow">
           <span className="text-sm text-gray-400 mb-1">Luck</span>
-          <Progress w={200} value={luck} className="bg-green-500" />
+          <Progress
+            w="full"
+            value={luck}
+            className="bg-green-500"
+          />
         </div>
       </div>
     </div>
